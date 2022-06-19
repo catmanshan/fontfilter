6220ea6114541ec62eb7dc1cbd46bac287bd920b
catmanshan <catmanshan@gmail.com>
2022-06-18 22:35:26 -0600

    add types

--- src/fontfilter.h
+++ src/fontfilter.h
@@ -1,6 +1,57 @@
 #ifndef fontfilter_h
 #define fontfilter_h
 
-extern int stub;
+#include <stdbool.h>
+
+#include <fontconfig/fontconfig.h>
+
+typedef enum FfConditionType {
+	FF_COMPARISON,
+	FF_COMPOSITION
+} FfConditionType;
+
+typedef enum FfOperation {
+	FF_NOT_EQUAL = 0,
+	FF_EQUAL,
+	FF_LESS_THAN,
+	FF_GREATER_THAN,
+	FF_LESS_THAN_EQUAL,
+	FF_GREATER_THAN_EQUAL,
+} FfOperation;
+
+typedef struct FfTruthTable FfTruthTable;
+typedef struct FfComparison FfComparison;
+typedef struct FfComposition FfComposition;
+typedef union FfConditionValue FfConditionValue;
+typedef struct FfCondition FfCondition;
+
+struct FfTruthTable {
+	bool pt_qt;
+	bool pt_qf;
+	bool pf_qt;
+	bool pf_qf;
+};
+
+struct FfComparison {
+	const char *object;
+	FcValue value;
+	FfOperation operation;
+};
+
+struct FfComposition {
+	FfTruthTable truth_table;
+	FfCondition *p;
+	FfCondition *q;
+};
+
+union FfConditionValue {
+	FfComparison comparison;
+	FfComposition composition;
+};
+
+struct FfCondition {
+	FfConditionType type;
+	FfConditionValue value;
+};
 
 #endif // fontfilter_h
